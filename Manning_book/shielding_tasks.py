e40ad807ded94d4eea5d69373b09d0a789d729ff
I T <ivo.a.v.tavares@gmail.com>
2022-11-07 00:36:44 +0000

    added example with shielded task.

---
+++ Manning_book/shielding_tasks.py
@@ -0,0 +1,15 @@
+import asyncio
+from util.delay_functions import delay
+
+async def main():
+    task = asyncio.create_task(delay(10))
+    try:
+        result = await asyncio.wait_for(asyncio.shield(task), 5)
+        print(result)
+    except asyncio.exceptions.TimeoutError:
+        print("Task took longer than five seconds, it will finish soon!")
+        # the shield will make .cancel method to be ineffective!  
+        result = await task
+        print(result)
+
+asyncio.run(main())
